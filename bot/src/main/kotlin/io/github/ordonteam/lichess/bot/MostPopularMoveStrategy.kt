f5efe151f6542f153fffa2102fd43ed997617cdb
Karol Kowalski <ordon1313@gmail.com>
2021-02-28 12:50:00 +0100

    Inline nextMoveFromDisk method

--- bot/src/main/kotlin/io/github/ordonteam/lichess/bot/MostPopularMoveStrategy.kt
+++ bot/src/main/kotlin/io/github/ordonteam/lichess/bot/MostPopularMoveStrategy.kt
@@ -3,11 +3,8 @@ package io.github.ordonteam.lichess.bot
 import io.github.ordonteam.lichess.bot.BotStrategy.LichessGame
 
 class MostPopularMoveStrategy(private val movesProvider: MovesProvider) : BotStrategy {
-    override fun nextMove(lichessGame: LichessGame): String? {
-        return nextMoveFromDisk(lichessGame.fen)
-    }
 
-    private fun nextMoveFromDisk(fen: String): String? {
-        return movesProvider.getMovesForFen(fen).maxByOrNull { it.value.all }?.key
+    override fun nextMove(lichessGame: LichessGame): String? {
+        return movesProvider.getMovesForFen(lichessGame.fen).maxByOrNull { it.value.all }?.key
     }
 }
