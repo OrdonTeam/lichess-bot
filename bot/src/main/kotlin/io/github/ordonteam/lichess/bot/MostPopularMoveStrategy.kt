b9cfeab6db08641290334f463634bbb0e05f30ef
Karol Kowalski <ordon1313@gmail.com>
2021-02-28 21:51:41 +0100

    One of the most popular moves

--- bot/src/main/kotlin/io/github/ordonteam/lichess/bot/MostPopularMoveStrategy.kt
+++ bot/src/main/kotlin/io/github/ordonteam/lichess/bot/MostPopularMoveStrategy.kt
@@ -5,7 +5,12 @@ import io.github.ordonteam.lichess.bot.BotStrategy.LichessGame
 class MostPopularMoveStrategy(private val movesProvider: MovesProvider) : BotStrategy {
 
     override fun nextMove(lichessGame: LichessGame): String? {
-        return movesProvider.getMovesForFen(lichessGame.fen).maxByOrNull { it.value.all }?.key
+        val movesForFen = movesProvider.getMovesForFen(lichessGame.fen).map { it.key to it.value }
+        if (movesForFen.isEmpty()) {
+            return null
+        }
+        val maxCount = movesForFen.maxByOrNull { it.second.all }!!.second.all
+        return movesForFen.filter { it.second.all * 10 > maxCount }.random().first
     }
 
     override fun chatLine(playerId: String, text: String) = Unit
